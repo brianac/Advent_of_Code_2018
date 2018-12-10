1012ce873f250fd24820620ab16e29762e538416
Briana Coppard <brianacoppard@gmail.com>
2018-12-10 20:08:05 +1100

    day 9 optimized

--- day9/2.js
+++ day9/2.js
@@ -2,40 +2,71 @@ const data = '455 players; last marble is worth 71223 points'
 
 const splitData = data.split(' ')
 
+class LinkedList {
+  constructor() {
+    this.currentNode = new Node(0);
+    this.currentNode.prev = this.currentNode;
+    this.currentNode.next = this.currentNode;
+  }
+
+  addNode(value) {
+    let node = this.currentNode
+
+    for(let i = 0; i < 1; i++) {
+      node = node.next
+    }
+    const newNode = new Node(value, node, node.next)
+
+    node.next.prev = newNode
+    node.next = newNode
+    
+    this.currentNode = newNode
+  }
+
+  removeNode() {
+    let node = this.currentNode
+
+    for(let i = 0; i < 7; i++) {
+      node = node.prev
+    }
+
+    const prevNode = node.prev
+    const nextNode = node.next
+
+    prevNode.next = nextNode
+    nextNode.prev = prevNode
+
+    this.currentNode = nextNode
+
+    return node.value
+  }
+}
+
+class Node {
+  constructor(value, prev = null, next = null) {
+    this.value = value;
+    this.prev = prev;
+    this.next = next;
+  }
+}
+
 const players = parseInt(splitData[0])
 const lastMarble = parseInt(splitData[6])*100
-const grid = [0]
 let lastIndex = 0
 let scores = {}
+const list = new LinkedList()
 
 for (let i = 1; i <= lastMarble; i++) {
 	if (i%23 === 0) {
-        const player = i%players+1
-        
-        console.log(i, lastMarble)
-
-		lastIndex -= 7
-
-		if (lastIndex < 0) {
-			lastIndex = grid.length + lastIndex
-		}
-
-		const value = i + grid[lastIndex]
-
-		grid.splice(lastIndex, 1)
+    const player = i%players+1
+		const value = i + list.removeNode()
 
 		if (!scores[player]) {
 			scores[player] = 0
 		}
     scores[player] += value
 	} else {
-		lastIndex += 2
-
-		if (grid.length < lastIndex) {
-			lastIndex = 1
-		}
-	
-		grid.splice(lastIndex, 0, i)
+    list.addNode(i)
 	}
 }
 
